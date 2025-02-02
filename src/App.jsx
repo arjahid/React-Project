f94cfd0db164caacaba19bf13199274613489bbd
Jahid-Hasan <jhjahidhasanar@gmail.com>
2025-02-02 19:46:41 +0600

    added navbar

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+
+import './App.css'
+import NavBar from './components/NavBar/NavBar'
+
+// import DaisyNav from './components/DaisyNav/DaisyNav'
+
+function App() {
+  
+
+  return (
+    <>
+     <NavBar></NavBar>
+      <h1 className='te'>Vite + React</h1>
+     {/* <DaisyNav></DaisyNav> */}
+    </>
+  )
+}
+
+export default App
